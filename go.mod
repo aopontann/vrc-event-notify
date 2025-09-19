992de0a454bf2bb331645a712adde49d806adf8d
aopontann <aopontan0416@gmail.com>
2025-09-19 22:43:22 +0900

    mongoDBを使用するように修正

--- go.mod
+++ go.mod
@@ -3,31 +3,34 @@ module github.com/aopontann/vrc-event-notify
 go 1.25.1
 
 require (
-	cloud.google.com/go/firestore v1.18.0
+	github.com/GoogleCloudPlatform/functions-framework-go v1.9.2
 	github.com/joho/godotenv v1.5.1
+	go.mongodb.org/mongo-driver/v2 v2.3.0
 	google.golang.org/api v0.249.0
 )
 
 require (
-	cloud.google.com/go v0.120.0 // indirect
 	cloud.google.com/go/auth v0.16.5 // indirect
 	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
 	cloud.google.com/go/compute/metadata v0.8.0 // indirect
-	cloud.google.com/go/longrunning v0.6.7 // indirect
-	github.com/GoogleCloudPlatform/functions-framework-go v1.9.2 // indirect
 	github.com/cloudevents/sdk-go/v2 v2.15.2 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/s2a-go v0.1.9 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
 	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.61.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
 	go.opentelemetry.io/otel v1.37.0 // indirect
 	go.opentelemetry.io/otel/metric v1.37.0 // indirect
@@ -41,9 +44,6 @@ require (
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
-	golang.org/x/time v0.12.0 // indirect
-	google.golang.org/genproto v0.0.0-20250603155806-513f23925822 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250818200422-3122310a409c // indirect
 	google.golang.org/grpc v1.75.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
--- main.go
+++ main.go
@@ -6,22 +6,23 @@ import (
 	"os"
 	"time"
 
-	"cloud.google.com/go/firestore"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/option"
 )
 
 type EventInfo struct {
-	Summary       string `firestore:"channel_id,omitempty"`
-	Description   string `firestore:"description,omitempty"`
-	StartDateTime string `firestore:"start_date_time,omitempty"`
-	EndDateTime   string `firestore:"end_date_time,omitempty"`
+	Summary       string `bson:"summary,omitempty"`
+	Description   string `bson:"description,omitempty"`
+	StartDateTime string `bson:"start_date_time,omitempty"`
+	EndDateTime   string `bson:"end_date_time,omitempty"`
 }
 
 func Main() error {
 	APIKey := os.Getenv("API_KEY")
 	calendarID := os.Getenv("CALENDAR_ID")
-	projectID := os.Getenv("PROJECT_ID")
+	uri := os.Getenv("MONGODB_URI")
 
 	ctx := context.Background()
 	calendarService, err := calendar.NewService(ctx, option.WithAPIKey(APIKey))
@@ -30,29 +31,33 @@ func Main() error {
 		return err
 	}
 
-	client, err := firestore.NewClientWithDatabase(ctx, projectID, projectID)
+	// Uses the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	// Defines the options for the MongoDB client
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	// Creates a new client and connects to the server
+	client, err := mongo.Connect(opts)
 	if err != nil {
-		slog.Error("Failed to create Firestore client: " + err.Error())
-		return err
+		panic(err)
 	}
-	defer func(client *firestore.Client) {
-		err := client.Close()
-		if err != nil {
-			slog.Error("Failed to close Firestore client: " + err.Error())
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
 		}
-	}(client)
+	}()
 
 	// 24時間以内に更新されたカレンダーイベントを取得するための処理
 	updatedMin := time.Now().AddDate(0, 0, -1).Format(time.RFC3339)
 	slog.Info("updatedMin: " + updatedMin)
 
-	list := calendarService.Events.List(calendarID).MaxResults(5).OrderBy("updated").SingleEvents(true).UpdatedMin(updatedMin)
+	list := calendarService.Events.List(calendarID).MaxResults(50).OrderBy("updated").SingleEvents(true).UpdatedMin(updatedMin)
 	event, err := list.Do()
 	if err != nil {
 		slog.Error("failed to list events:: " + err.Error())
 		return err
 	}
 
+	eventInfo := make(map[string]EventInfo)
 	for _, i := range event.Items {
 		slog.Info("event",
 			slog.String("event_id", i.Id),
@@ -61,16 +66,17 @@ func Main() error {
 			slog.String("start_date_time", i.Start.DateTime),
 			slog.String("end_date_time", i.End.DateTime),
 		)
-		e := &EventInfo{
+		eventInfo[i.Id] = EventInfo{
 			Summary:       i.Summary,
 			Description:   i.Description,
 			StartDateTime: i.Start.DateTime,
 			EndDateTime:   i.End.DateTime,
 		}
-		_, err := client.Collection("events").Doc(i.Id).Set(ctx, e)
-		if err != nil {
-			slog.Error("failed to add event: " + err.Error())
-		}
+	}
+
+	_, err = client.Database("mongo-db").Collection("events").InsertMany(ctx, eventInfo)
+	if err != nil {
+		slog.Error("failed to add event: " + err.Error())
 	}
 
 	return nil
